ccdbef8c6b5a6eab96241e1b7f496df0c66b096f
stermic2 <46682781+stermic2@users.noreply.github.com>
2020-10-26 17:02:21 -0600

    axios added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :list-data="users"/>
   </div>
 </template>
 
@@ -12,6 +12,16 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      users: []
+    }
+  },
+  mounted() {
+    this.$api.get('resume-bodies').then(response => {
+      this.users = response.data
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.use({
+  install (Vue) {
+    Vue.prototype.$api = axios.create({
+      baseURL: 'https://sterner-strapi.herokuapp.com/'
+    })
+  }
+})
 
 new Vue({
   render: h => h(App),
