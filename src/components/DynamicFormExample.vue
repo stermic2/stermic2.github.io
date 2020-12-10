9e18912c591a59a0053d24226f5c3d32ad9fe3c5
stermic2 <46682781+stermic2@users.noreply.github.com>
2020-12-10 15:07:44 -0700

    Dynamic form modifyable through front end

--- src/components/DynamicFormExample.vue
+++ src/components/DynamicFormExample.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-    <div class="container">
       <h1 class="title text-center">Dynamic Forms Example</h1>
-      <h1 class="text-center">Notice: the current setup below is a demo that I didn't write... I'm currently experimenting with this.</h1>
+    <pre>{{ formData }}</pre>
       <div class="row mt-5">
+        <div class="col-6">
+          <div v-for="(field, i) in testForm.fields" :key="i">
+              <label for="type">type</label>
+              <select v-model="testForm.fields[i].type" id="type">
+                <option v-for="option in options" v-bind:value="option.value" v-bind:key="option.value">
+                  {{option.text}}
+                </option>
+              </select>
+              <p>
+                <label for="name">name</label>
+                <input v-model="testForm.fields[i].name" id="name"/>
+                <label for="label">label</label>
+                <input v-model="testForm.fields[i].label" id="label"/>
+              </p>
+          </div>
+        </div>
         <div class="col-6">
           <dynamic-form :id="testForm.id" :fields="testForm.fields" @change="valuesChanged"/>
           <div class="row d-flex justify-content-end p-4">
             <button submit="true" :form="testForm.id" class="btn btn-primary">Submit</button>
           </div>
         </div>
-        <div class="col-6">
-          <pre>{{ formData }}</pre>
-          <pre>{{ testForm }}</pre>
-        </div>
       </div>
-    </div>
   </div>
 </template>
 
@@ -28,90 +38,12 @@ import {
   pattern
 } from "@asigloo/vue-dynamic-forms";
 
-const data = () => ({
-  formData: {},
-  testForm: {
-    id: "test-form",
-    fields: [
-      new FormField({
-        type: "text",
-        label: "Name",
-        name: "name"
-      }),
-      new FormField({
-        type: "email",
-        label: "Email",
-        name: "email",
-        validations: [
-          new FormValidation(required, "This field is required"),
-          new FormValidation(email, "Format of email is incorrect")
-        ]
-      }),
-      new FormField({
-        type: "password",
-        label: "Password",
-        name: "password",
-        validations: [
-          new FormValidation(required, "This field is required"),
-          new FormValidation(
-              pattern(
-                  "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[#$^+=!*()@%&]).{8,10}$"
-              ),
-              "Password must contain at least 1 Uppercase, 1 Lowercase, 1 number, 1 special character and min 8 characters max 10"
-          )
-        ],
 
-        value: "sdsdsd"
-      }),
-      new FormField({
-        type: "textarea",
-        label: "Bio",
-        name: "bio",
-        cols: 30,
-        rows: 5
-      }),
-      new FormField({
-        type: "select",
-        label: "Category",
-        name: "category",
-        options: [
-          { value: null, text: "Please select an option" },
-          { value: "arduino", text: "Arduino" },
-          { value: "transistors", text: "Transistors" }
-        ]
-      }),
-      new FormField({
-        type: "checkbox",
-        label: "Read the conditions",
-        name: "conditions",
-        inline: false
-      }),
-      new FormField({
-        type: "radio",
-        label: "Prefered Animal",
-        name: "animal",
-        inline: true,
-        options: [
-          { text: "Dogs", value: "dogs" },
-          { text: "Cats", value: "cats" },
-          { text: "Others", value: "others" }
-        ]
-      }),
-      new FormField({
-        type: "number",
-        label: "Number",
-        name: "number",
-        value: 0
-      })
-    ]
-  }
-});
 
 const methods = {
   valuesChanged(values) {
-    this.$forceUpdate(); // this is only to refresh the fields on the <pre> tags, not necessary for other purpouses
+    this.$forceUpdate();
     this.formData = {
-      ...this.formData,
       ...values
     };
   }
@@ -119,7 +51,94 @@ const methods = {
 
 export default {
   name: "App",
-  data,
+  data () { return {
+    formData: {},
+    options: [
+      { text: 'Text', value: 'text' },
+      { text: 'Email', value: 'email' },
+      { text: 'Password', value: 'password' },
+      { text: 'Text Area', value: 'textarea' },
+      { text: 'Select', value: 'select' },
+      { text: 'Checkbox', value: 'checkbox' },
+      { text: 'Radio', value: 'radio' },
+      { text: 'Number', value: 'number' }
+    ],
+    testForm: {
+      id: "test-form",
+      fields: [
+        new FormField({
+          type: "text",
+          label: "Name",
+          name: "name"
+        }),
+        new FormField({
+          type: "email",
+          label: "Email",
+          name: "email",
+          validations: [
+            new FormValidation(required, "This field is required"),
+            new FormValidation(email, "Format of email is incorrect")
+          ]
+        }),
+        new FormField({
+          type: "password",
+          label: "Password",
+          name: "password",
+          validations: [
+            new FormValidation(required, "This field is required"),
+            new FormValidation(
+                pattern(
+                    "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[#$^+=!*()@%&]).{8,10}$"
+                ),
+                "Password must contain at least 1 Uppercase, 1 Lowercase, 1 number, 1 special character and min 8 characters max 10"
+            )
+          ],
+
+          value: "sdsdsd"
+        }),
+        new FormField({
+          type: "textarea",
+          label: "Bio",
+          name: "bio",
+          cols: 30,
+          rows: 5
+        }),
+        new FormField({
+          type: "select",
+          label: "Category",
+          name: "category",
+          options: [
+            { value: null, text: "Please select an option" },
+            { value: "arduino", text: "Arduino" },
+            { value: "transistors", text: "Transistors" }
+          ]
+        }),
+        new FormField({
+          type: "checkbox",
+          label: "Read the conditions",
+          name: "conditions",
+          inline: false
+        }),
+        new FormField({
+          type: "radio",
+          label: "Prefered Animal",
+          name: "animal",
+          inline: true,
+          options: [
+            { text: "Dogs", value: "dogs" },
+            { text: "Cats", value: "cats" },
+            { text: "Others", value: "others" }
+          ]
+        }),
+        new FormField({
+          type: "number",
+          label: "Number",
+          name: "number",
+          value: 0
+        })
+      ]
+    }
+  }},
   methods
 };
 </script>
