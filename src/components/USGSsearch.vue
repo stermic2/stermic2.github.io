8ceb7da3e7316071ba7575399e6370161a9d8287
stermic2 <46682781+stermic2@users.noreply.github.com>
2020-12-15 17:04:25 -0700

    USGS search added

--- src/components/USGSsearch.vue
+++ src/components/USGSsearch.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="container">
-    <div id="map1"></div>
+    <div id="map1"    class="map"   ></div>
+    <div id="search2" class="search"></div>
   </div>
 </template>
-
-<!-- leaflet web map api -->
-<link    rel="stylesheet"     href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.2/leaflet.css"/>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.2/leaflet.js"></script>
 
 <script>
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import $ from "jquery";
 
 export default {
   name: "Map",
   methods: {
-    setupLeafletMap: () => {
-      new L.Map( "map1", {
+    setupLeafletMap: function() {
+      this.map = new L.Map( "map1", {
         center  : new L.LatLng(38,-99),
         minZoom : 3,
         maxZoom : 15,
         zoom    : 3,
         layers  : L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
         searchControl : {} // can specify options - those unspecified set to defaults
+      });
+      // eslint-disable-next-line no-undef
+      search_api.create( "search2", {
+        on_result: function(o) {
+          // what to do when a location is found
+          // o.result is geojson point feature of location with properties
+          this.map
+              .fitBounds([ // zoom to location
+                [ o.result.properties.LatMin, o.result.properties.LonMin ],
+                [ o.result.properties.LatMax, o.result.properties.LonMax ]
+              ])
+              .openPopup(  // open popup at location listing all properties
+                  $.map( Object.keys(o.result.properties), function(property) {
+                    return "<b>" + property + ": </b>" + o.result.properties[property];
+                  }).join("<br/>"),
+                  [ o.result.properties.Lat, o.result.properties.Lon ]
+              );
+        }
       });
     },
   },
