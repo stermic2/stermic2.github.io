aff8c12de28cf43b7b7edf4e5eee34f10c6de535
stermic2 <46682781+stermic2@users.noreply.github.com>
2021-02-05 11:26:59 -0700

    DynamicCQ page added

---
+++ src/components/DynamicCQ.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1 class="title text-center">DynamicCQ</h1>
+    <div class="row mt-5">
+      <div class="col-12">
+        <p>
+          Hello, I'm still working on this.  You might have received a link to this page... If so, what it is meant to be is documentation for a Mediatr Library dependent on Autofac, Automapper, and .Net Core v5.
+          I plan to have it available before the end of Feb. 2021.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "DynamicCQ"
+}
+</script>
+
+<style scoped>
+
+</style>
